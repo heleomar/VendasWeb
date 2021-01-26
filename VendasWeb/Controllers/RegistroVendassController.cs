147fda0c65b08465ee516b70229cb29199f33087
Heleomar <mazinhoemail@gmail.com>
2021-01-26 10:56:09 -0300

    Telas de navegacao e busca

---
+++ VendasWeb/Controllers/RegistroVendassController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWeb.Controllers
+{
+    public class RegistroVendassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
+
--- VendasWeb/Services/Exceptions/IntegrityException.cs
+++ VendasWeb/Services/Exceptions/IntegrityException.cs
@@ -12,4 +12,4 @@ namespace VendasWeb.Services.Exceptions
         }
     }
 }
-}
+
